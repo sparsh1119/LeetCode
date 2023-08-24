55065217fa35dffdce5937be83d661a4130ec614
Sparsh <117457293+sparsh1119@users.noreply.github.com>
2023-08-25 00:49:35 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (74.79%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,14 @@
+vector<int>dp(46,-1);
+class Solution {
+public:
+    
+    int climbStairs(int n) {
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        if(n<=2)
+            return dp[n]= n;
+        
+        return dp[n] = climbStairs(n-1)+climbStairs(n-2);
+    }
+};
