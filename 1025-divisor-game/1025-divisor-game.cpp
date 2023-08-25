dbef4ebf2bccfb5c313e2c49d0d715ed05830a37
Sparsh <117457293+sparsh1119@users.noreply.github.com>
2023-08-25 23:19:11 +0530

    Time: 0 ms (100.00%), Space: 6 MB (50.82%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
     bool divisorGame(int n) {
-        // if(n%2==0){
-        //     return true;
-        // }
-        // else{
-        //     return false;
-        
-        
-        //recursive solution
-        if(n<=1) return false;
-        for(int x=1; x<n; x++){
-            if(n%x==0){
-                return !divisorGame(n-x);
-            }
+        if(n%2==0){
+            return true;
         }
-        return false;
-        
+        else{
+            return false;
+        }
     }
 };
