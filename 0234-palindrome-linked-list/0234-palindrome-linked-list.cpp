487b94c2e4162bde85d91f6a601807c83f84666e
Sparsh <117457293+sparsh1119@users.noreply.github.com>
2023-11-03 23:53:05 +0530

    Time: 135 ms (98.06%), Space: 110.7 MB (96.95%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        //BASE CASE
+        if(head->next == nullptr) return true;
+
+        ListNode *fast = head;
+        ListNode *slow = head;
+        ListNode *prev = NULL;
+
+        // Finding Mid Using 2 pointer approach
+        // Reversing the first half of linked list
+        while(fast && fast->next != NULL){
+            head = head->next;
+            fast = fast->next->next;
+            slow->next = prev;
+            prev = slow;
+            slow = head;
+        }
+
+        // If the length of linked list is odd
+        if(fast != NULL){
+            head = head->next;
+        } 
+
+        // Checking for palindrome
+        while(head != nullptr){
+            if(head->val != prev->val) return false;
+            head = head->next;
+            prev = prev->next;
+        }
+
+        return true;
+
+    }
+};
+
+
+
+
+
+
