52c4074d909df42893631d9aa66dfcfb9c9ad246
Sparsh <117457293+sparsh1119@users.noreply.github.com>
2024-02-08 00:50:08 +0530

    Time: 67 ms (22.69%), Space: 51.5 MB (21.35%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,31 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let count = init || 0;
+    
+    const increment = () =>{
+        count++;
+        return count;
+    };
+    
+    const decrement = () =>{
+        count--;
+        return count;
+    };
+    
+    const reset = () => {
+        count = init;
+        return count;
+    };
+    
+    return {increment , decrement , reset}
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
