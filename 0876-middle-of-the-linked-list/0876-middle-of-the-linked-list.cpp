c7677eb3a976d4725519b3baa473d538dcf0481b
Sparsh <117457293+sparsh1119@users.noreply.github.com>
2024-03-07 18:14:59 +0530

    Time: 4 ms (30.24%), Space: 8.5 MB (66.29%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -11,14 +11,14 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-
         ListNode* slow = head;
         ListNode* fast = head;
-
-        while( fast != NULL && fast->next != NULL){
-            slow = slow -> next;
-            fast = fast -> next -> next;  
-         }
+        
+        while(fast != nullptr && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
         return slow;
     }
 };
