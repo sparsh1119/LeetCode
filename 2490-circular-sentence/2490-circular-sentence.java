0f1b5ce05dc9b9306e4bd0fb08fc23f87c342eac
Sparsh <117457293+sparsh1119@users.noreply.github.com>
2024-11-03 01:22:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2490-circular-sentence/2490-circular-sentence.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isCircularSentence(String sentence) {
+        String[] arr = sentence.split(" ");
+        
+        // Check each word's last character with the next word's first character
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (!arr[i].substring(arr[i].length() - 1).equalsIgnoreCase(String.valueOf(arr[i + 1].charAt(0)))) {
+                return false;
+            }
+        }
+
+        // Check the last word's last character with the first word's first character
+        if (!arr[arr.length - 1].substring(arr[arr.length - 1].length() - 1)
+                .equalsIgnoreCase(String.valueOf(arr[0].charAt(0)))) {
+            return false;
+        }
+
+        return true;
+    }
+}
